1d32996ac07fd7001419a8558141e0b826ebbfe7
RS <rsphenomenal@gmail.com>
2025-02-12 03:28:08 +0530

    fix for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['unsplash.com', 'aceternity.com'], // Add the hostname causing the error here
+    domains: ['unsplash.com', 'aceternity.com'],
   },
 };
-
 export default nextConfig;
