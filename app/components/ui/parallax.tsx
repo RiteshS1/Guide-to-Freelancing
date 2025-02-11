6045a4b71d8e32f9ccb4179e34dd6458824b4e54
Karan Chouhan <129978415+KaranChouhan018@users.noreply.github.com>
2025-02-12 03:51:34 +0530

    Update parallax.tsx

--- app/components/ui/parallax.tsx
+++ app/components/ui/parallax.tsx
@@ -57,7 +57,7 @@ export const Parallax = ({
   return (
     <div
       ref={ref}
-      className="h-[250vh] py-40 overflow-hidden bg-black  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-screen py-40 overflow-hidden bg-black  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
