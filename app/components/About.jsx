371626f7309c70cdebb4607b8fff28722fae5ed3
David Nguyen <davido.nguyen1212@gmail.com>
2025-02-12 22:05:40 -0800

    about and contact buttons lead to correct sections

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,36 +5,38 @@ import { LampContainer } from "./ui/lamp";
 
 export function LampDemo() {
   return (
-    <div className="bg-black h-full">
-    <LampContainer>
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 10 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-      >
-        About Us
-      </motion.h1>
-      <motion.p
-        initial={{ opacity: 0.5, y: 50 }}
-        whileInView={{ opacity: 1, y: 100 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-4 text-center bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text font-medium tracking-tight text-transparent  md:text-xl"
-      >
-        We created this platform to help freelancers find the best opportunities and resources.
-        <br />
-        Our website provides a comprehensive guide to freelancing, featuring blogs, videos, and more to help you succeed in your freelancing career.
-      </motion.p>
-    </LampContainer>
-    </div>
+    <section id="About">
+      <div className="bg-black h-full">
+      <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 10 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        >
+          About Us
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0.5, y: 50 }}
+          whileInView={{ opacity: 1, y: 100 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-4 text-center bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text font-medium tracking-tight text-transparent  md:text-xl"
+        >
+          We created this platform to help freelancers find the best opportunities and resources.
+          <br />
+          Our website provides a comprehensive guide to freelancing, featuring blogs, videos, and more to help you succeed in your freelancing career.
+        </motion.p>
+      </LampContainer>
+      </div>
+    </section>
   );
 }
 export default LampDemo;
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -12,39 +12,41 @@ const Footer = () => {
   };
 
   return (
-    <footer className="p-8 bg-black text-white">
-      <div className="h-[40rem] w-full rounded-md bg-black relative flex flex-col items-center justify-center antialiased">
-        <div className="max-w-2xl mx-auto p-4">
-          <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
-            Contribute to Open Source
-          </h1>
-          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem nobis consequuntur rem ut facilis fuga ea excepturi ipsa quo cum minus doloribus sequi, commodi, dolorem nam. Temporibus debitis est soluta ex ipsum quasi molestias ipsa possimus, tempore consequuntur. Laudantium recusandae necessitatibus iste rem quod vitae incidunt.
-          </p>
-          <form onSubmit={handleSubmit} className="relative z-10 mt-4 flex flex-col items-center">
-            <input
-              type="text"
-              placeholder="@delphic_RS"
-              value={suggestion}
-              onChange={(e) => setSuggestion(e.target.value)}
-              className="rounded-lg p-2 border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full bg-neutral-950 placeholder:text-neutral-700"
-            />
-            <button
-              type="submit"
-              className="mt-2 px-4 py-2 text-gray-300 rounded-full bg-gradient-to-b from-neutral-300 to-neutral-600 text-center transition-colors hover:text-black"
-            >
-              Submit
-            </button>
-          </form>
+    <section id="Contact">
+      <footer className="p-8 bg-black text-white">
+        <div className="h-[40rem] w-full rounded-md bg-black relative flex flex-col items-center justify-center antialiased">
+          <div className="max-w-2xl mx-auto p-4">
+            <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
+              Contribute to Open Source
+            </h1>
+            <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem nobis consequuntur rem ut facilis fuga ea excepturi ipsa quo cum minus doloribus sequi, commodi, dolorem nam. Temporibus debitis est soluta ex ipsum quasi molestias ipsa possimus, tempore consequuntur. Laudantium recusandae necessitatibus iste rem quod vitae incidunt.
+            </p>
+            <form onSubmit={handleSubmit} className="relative z-10 mt-4 flex flex-col items-center">
+              <input
+                type="text"
+                placeholder="@delphic_RS"
+                value={suggestion}
+                onChange={(e) => setSuggestion(e.target.value)}
+                className="rounded-lg p-2 border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full bg-neutral-950 placeholder:text-neutral-700"
+              />
+              <button
+                type="submit"
+                className="mt-2 px-4 py-2 text-gray-300 rounded-full bg-gradient-to-b from-neutral-300 to-neutral-600 text-center transition-colors hover:text-black"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+          <BackgroundBeams />
         </div>
-        <BackgroundBeams />
-      </div>
-      <div className='text-center'>
-        <h2>
-          Made with 🤍 by RS
-        </h2>
-      </div>
-    </footer>
+        <div className='text-center'>
+          <h2>
+            Made with 🤍 by RS
+          </h2>
+        </div>
+      </footer>
+    </section>
   );
 };
 
